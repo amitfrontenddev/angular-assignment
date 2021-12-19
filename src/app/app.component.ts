f03795242abee90ea48f019812e7ed40a0f87a3e
amitfrontenddev <amitfrontenddev@gmail.com>
2021-12-19 12:02:40 +0530

    4 files modified, 2 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,73 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from './models/user';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent implements OnInit {
+  users: User[];
+  userForm: boolean;
+  isNewUser: boolean;
+  newUser: any = {};
+  editUserForm: boolean;
+  editedUser: any = {};
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.users = this.getUsers();
+  }
+
+  getUsers(): User[] {
+    return this.userService.getUsersFromData();
+  }
+
+  showEditUserForm(user: User) {
+    if (!user) {
+      this.userForm = false;
+      return;
+    }
+    this.editUserForm = true;
+    this.editedUser = user;
+  }
+
+  showAddUserForm() {
+    // resets form if edited user
+    if (this.users.length) {
+      this.newUser = {};
+    }
+    this.userForm = true;
+    this.isNewUser = true;
+  }
+
+  saveUser(user: User) {
+    if (this.isNewUser) {
+      // add a new user
+      this.userService.addUser(user);
+    }
+    this.userForm = false;
+  }
+
+  updateUser() {
+    this.userService.updateUser(this.editedUser);
+    this.editUserForm = false;
+    this.editedUser = {};
+  }
+
+  removeUser(user: User) {
+    this.userService.deleteUser(user);
+  }
+
+  cancelEdits() {
+    this.editedUser = {};
+    this.editUserForm = false;
+  }
+
+  cancelNewUser() {
+    this.newUser = {};
+    this.userForm = false;
+  }
 }
